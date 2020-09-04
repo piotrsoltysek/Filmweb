933b5ce8e638294783be2c91c58ebaa50d109125
Piotr Sołtysek <p.soltysek@gmail.com>
2020-09-04 11:32:15 +0200

    first commit

---
+++ src/main/java/pl/camp/it/filmweb/App.java
@@ -0,0 +1,7 @@
+package pl.camp.it.filmweb;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/pl/camp/it/filmweb/AppConfiguration.java
@@ -0,0 +1,4 @@
+package pl.camp.it.filmweb;
+
+public class AppConfiguration {
+}
